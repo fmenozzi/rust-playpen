75f29a1a438c88dc8755bd3187abd419694eed45
Federico Menozzi <federicogmenozzi@gmail.com>
2017-04-11 22:09:16 -0400

    Revert "Use Copy instead of Clone"
    This reverts commit 921ce0f4964928ec40032b18d081daf52fd60a4f.

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -2,12 +2,12 @@ extern crate partition;
 
 use partition::partition;
 
-pub fn quicksort<T: PartialOrd + Copy>(a: &mut[T]) {
+pub fn quicksort<T: PartialOrd + Clone>(a: &mut[T]) {
     if a.len() <= 1 {
         return
     }
 
-    let pivot = a[0];
+    let pivot = a[0].clone();
     let (mut smaller, mut greater_equal) = partition(&mut a[..], |x| x < &pivot);
     let (_, mut greater) = partition(&mut greater_equal, |x| x == &pivot);
 
