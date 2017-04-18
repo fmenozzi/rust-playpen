a281681430e3321061897e65329db9f985507bff
Federico Menozzi <federicogmenozzi@gmail.com>
2017-04-18 09:21:02 -0400

    Format wc output to be more like GNU wc

--- cli-utils/src/wc.rs
+++ cli-utils/src/wc.rs
@@ -3,6 +3,7 @@ use clap::ArgMatches;
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::cmp::max;
 
 pub fn wc(matches: &ArgMatches) {
     let wc_matches = matches.subcommand_matches("wc").unwrap();
@@ -19,23 +20,31 @@ pub fn wc(matches: &ArgMatches) {
                 num_chars += line.chars().count() + 1;
             }
 
+            let max_num = max(num_lines, max(num_words, num_chars));
+            let max_len = max_num.to_string().chars().count();
+
+            let width = max_len;
+
             let count_lines = wc_matches.is_present("lines") as i32;
             let count_words = wc_matches.is_present("words") as i32;
             let count_chars = wc_matches.is_present("chars") as i32;
 
-            let lwc_combo = format!("{}{}{}", count_lines, count_words, count_chars);
-            match lwc_combo.as_ref() {
+            let lwc = format!("{}{}{}", count_lines, count_words, count_chars);
+
+            match lwc.as_ref() {
                 "001" => println!("{} {}", num_chars, filename),
                 "010" => println!("{} {}", num_words, filename),
                 "100" => println!("{} {}", num_lines, filename),
-                "011" => println!("{} {} {}", num_words, num_chars, filename),
-                "101" => println!("{} {} {}", num_lines, num_chars, filename),
-                "110" => println!("{} {} {}", num_lines, num_words, filename),
+
+                "011" => println!("{:>width$} {:>width$} {}", num_words, num_chars, filename, width=width),
+                "101" => println!("{:>width$} {:>width$} {}", num_lines, num_chars, filename, width=width),
+                "110" => println!("{:>width$} {:>width$} {}", num_lines, num_words, filename, width=width),
 
                 "000" | "111" =>
-                    println!("{} {} {} {}", num_lines, num_words, num_chars, filename),
+                    println!("{:>width$} {:>width$} {:>width$} {}",
+                             num_lines, num_words, num_chars, filename, width=width),
 
-                _    => (),
+                _ => (),
             }
         },
         Err(err) => {
